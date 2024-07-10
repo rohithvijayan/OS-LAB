f74e4429c9aece9228bc91d81ec03d5da365a2a9
rohithvijayan <rohithvij20@gmail.com>
2024-07-11 02:09:26 +0530

    ADDED NEW PROGRAM

--- Shared-Memmory/sender.c
+++ Shared-Memmory/sender.c
@@ -7,11 +7,6 @@ key_t key=ftok(".",70);
 int shmid=shmget(key,1024,0666|IPC_CREAT);
 char *str=(char*)shmat(shmid,(void*)0,0);
 printf("Input Data :");
-fgets(str);
-if(shmdt(str)==0)
-{
-printf("Deattached...");}
-else
-{
-printf("Error");}
+fgets(str,1024,stdin);
+
 }
